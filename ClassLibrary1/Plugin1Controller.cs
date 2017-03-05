02c17de0f837913641e8ab9183d2fa4798d27836
ram-synechron <Ramachandran.Veluthattil@ingramcontent.com>
2017-03-04 23:18:58 -0600

    initial commit\

---
+++ ClassLibrary1/Plugin1Controller.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.Composition;
+
+namespace ClassLibrary1.Controllers
+{
+    [Export("Plugin1",typeof(IController))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class Plugin1Controller : Controller
+    {
+        
+        // GET: Plugin1
+        public ActionResult Index()
+        {
+            ViewBag.SimpleMessage = "Simple Message";
+            return View();
+        }
+
+        // GET: Plugin1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Plugin1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Plugin1/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Plugin1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Plugin1/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Plugin1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Plugin1/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MEF1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MEF1.MvcApplication" Language="C#" %>
